326362616ba198126aa5a8e9ee1dc37a40acf31e
coluccifrancesco <coluccif2002@gmail.com>
2025-06-03 15:24:39 +0200

    Add detailed comments to script.js files for Dispari and Pali sections

--- Dispari/js/script.js
+++ Dispari/js/script.js
@@ -1,1 +1,18 @@
 //  http://127.0.0.1:3000/Dispari/index.html
+
+/*
+    L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. 
+    Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
+    Sommiamo i due numeri. 
+    Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) 
+    Dichiariamo chi ha vinto.
+*/
+
+/*
+    Domande da farsi quando si crea una funzione:
+    
+    Come dovrebbe chiamarsi?
+    Ho bisogno di parametri?
+    Devo restituire un valore?
+    Se sì, di che tipo?
+*/
--- Pali/js/script.js
+++ Pali/js/script.js
@@ -1,1 +1,12 @@
 //  http://127.0.0.1:3000/Pali/index.html
+
+// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+/*
+    Domande da farsi quando si crea una funzione:
+    
+    Come dovrebbe chiamarsi?
+    Ho bisogno di parametri?
+    Devo restituire un valore?
+    Se sì, di che tipo?
+*/
