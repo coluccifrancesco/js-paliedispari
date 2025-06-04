1fc839fb810fd196678717ffce68712185482819
coluccifrancesco <coluccif2002@gmail.com>
2025-06-04 23:14:48 +0200

    done the code again to train, better result

--- Dispari/js/script.js
+++ Dispari/js/script.js
@@ -1,85 +1,54 @@
-//  http://127.0.0.1:3000/Dispari/index.html
-
-
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. 
-let userOddOrEven = prompt('Pari o dispari? (Ricorda la maiuscola!)').toLowerCase();
-console.log(userOddOrEven);
-
-// let userChoiceType = '';
-
-// if (userOddOrEven == 'pari' || userOddOrEven == 'dispari'){
-   
-//     userChoiceType = userOddOrEven;
-//     console.log("L'utente ha scelto: " + userChoiceType + '!');
-
-// } else {
-   
-//     userChoiceType = null;
-//     console.log('Riprova');
-
-// }
+let userOddEven = prompt('Inserisci pari o dispari');
+if (userOddEven != 'pari' && userOddEven != 'dispari'){
+    alert('Premi F5');
+}
 
 let userNumber = Number(prompt('Inserisci un numero da 1 a 5'));
-console.log(userNumber);
-// let userChoiceNumber = '';
-
-// if (userNumber < 1 || userNumber > 5){
- 
-//     userChoiceNumber = null;
-//     console.log('Non fare il fenomeno e leggi la richiesta');
-
-// } else {
-
-//     userChoiceNumber = userNumber;
-//     console.log("L'utente ha scelto il numero: " + userNumber + '!');
-
-// }
+if (userNumber < 1 || userNumber > 5){
+    alert('Premi F5');
+}
 
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
-function generateRandomNumber (min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+function computerNumber(){
+    let computerChoice = Math.floor(Math.random() * 4 + 1);
+    return computerChoice;
 }
 
-let computerChoice = generateRandomNumber(1,5);
-console.log(computerChoice);
+let computerRandom = computerNumber();
+console.log('Il computer ha scelto: ' + computerRandom);
 
-// Sommiamo i due numeri.
-let sum = userNumber + computerChoice; 
+// Sommiamo i due numeri 
+let sum = userNumber + computerRandom;
 console.log('La somma è: ' + sum);
-    
-    
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-function isEven(numb){
-    
-    if (numb % 2 == 0){
-    
-        return 'pari'
-    
+
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) 
+function isEven (){
+    let result = '';
+
+    if(sum % 2 === 0 && userOddEven === 'pari'){
+        result = 'Hai vinto';
+    } else if(sum % 2 !== 0 && userOddEven === 'dispari'){
+        result = 'Hai vinto';
+    } else {
+        result = 'Hai perso';
     }
 
-    return 'dispari'
+    liveVerdict(result);
 }
-
-let verdict = isEven(sum);
-console.log(verdict);
 
 // Dichiariamo chi ha vinto.
-if (verdict === userOddOrEven){
-    console.log('You Win')
-} else {
-    console.log('You lost')
+
+// let verdict = isEven();
+// console.log(verdict);
+
+function liveVerdict(result){
+
+    if(result === 'Hai vinto'){
+        console.log('Hai vinto tu campione!')
+    } else {
+        console.log('Hai perso!');
+    }
 }
 
-// if (verdict = true){
-   
-//     console.log('Hai vinto la scommessa')
-
-// } else if (verdict = false){
-
-//     console.log('Hai perso! Riprova premdendo F5')
-
-// } else if (userChoiceType == null || userChoiceNumber == null || computerChoice == null) {
-
-//     console.log('Hai sbagliato qualcosa, riprova')
-    
-// }
+isEven();
